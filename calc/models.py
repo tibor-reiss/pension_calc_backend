cdc6fa259fa27bea552be56f94e8ffb2773dd403
Tibor Reiss <tibor.reiss@gmail.com>
2024-06-08 20:30:36 +0200

    Change term to integer

--- calc/models.py
+++ calc/models.py
@@ -16,13 +16,13 @@ class PensionCalc(models.Model):
     monthly_contribution = models.DecimalField(max_digits=10, decimal_places=2)
     yearly_interest_rate = models.DecimalField(max_digits=10, decimal_places=2)
     starting_capital = models.DecimalField(max_digits=10, decimal_places=2)
-    term = models.DecimalField(max_digits=3, decimal_places=0)
+    term = models.IntegerField()
     create_time = models.DateTimeField(auto_now_add=True)
 
     @property
     def total_capital(self) -> Decimal:
         total = self.starting_capital
         monthly_rate = self.yearly_interest_rate / Decimal(12.0 * 100)
-        for _ in range(term * 12):
+        for _ in range(self.term * 12):
             total = total * (Decimal(1.0) + monthly_rate) + self.monthly_contribution
         return total
